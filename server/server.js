e56797646e0f58f6386ac136056d878b8b8e4d7a
Swarup Ranjan Mondal <swaranmon@gmail.com>
2020-07-05 18:23:48 +0530

    chat app committed

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const port = 3000;
+const io = require('socket.io')(port);
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        if(name == null){
+            return;
+        }
+        console.log(name + " joined");
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        const data = { name: users[socket.id], message: message };
+        socket.broadcast.emit('receive', data);
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('disconnected', users[socket.id]);
+        delete users[socket.id];
+    });
+});
+
+console.log(`Listening at port ${port}`);
