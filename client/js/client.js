ab98801bc1ed3e5a14c23522d773f086310481de
Swarup Ranjan Mondal <swaranmon@gmail.com>
2021-12-30 22:28:59 +0530

    changed the look & fixed a bug

--- client/js/client.js
+++ client/js/client.js
@@ -1,64 +1,65 @@
-const messageContainer = document.querySelector('.message-container');
-const inputText = document.getElementById('input-text');
-const form = document.querySelector('.input-container');
+const messageContainer = document.querySelector(".message-container");
+const inputText = document.getElementById("input-text");
+const form = document.querySelector(".input-container");
 
-const socket = io('http://localhost:3000');
+const socket = io("http://localhost:3000");
 
 const name = prompt("Enter your name");
 
-socket.emit('new-user', name);
+socket.emit("new-user", name);
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = inputText.value;
-    if (message == '') {
-        return;
-    }
-    socket.emit('send', message);
-    appendMessage('You: ' + message, 'right');
-    inputText.value = '';
-})
-
-socket.on('user-joined', name => {
-    const message = `${name} joined the Chat`;
-    appendMessage(message, 'middle');
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = inputText.value;
+  if (message == "") {
+    return;
+  }
+  socket.emit("send", message);
+  appendMessage("You: " + message, "right");
+  inputText.value = "";
 });
 
-socket.on('receive', data => {
-    const message = `${data.name}: ${data.message}`;
-    appendMessage(message, 'left');
+socket.on("user-joined", (name) => {
+  const message = `${name} joined the Chat`;
+  appendMessage(message, "middle");
 });
 
-socket.on('disconnected', name => {
-    const message = `${name} left the chat`;
-    appendMessage(message, 'middle');
+socket.on("receive", (data) => {
+  const message = `${data.name}: ${data.message}`;
+  appendMessage(message, "left");
+});
+
+socket.on("disconnected", (name) => {
+  const message = `${name} left the chat`;
+  appendMessage(message, "middle");
 });
 
 function appendMessage(message, position) {
-    const messageElement = document.createElement('div');
-    const nameElement = document.createElement('p');
-    const textElement = document.createElement('p');
-    const dateElement = document.createElement('p');
-    const date = (new Date()).toLocaleTimeString();
-    const index = date.lastIndexOf(':');
+  const messageElement = document.createElement("div");
+  const nameElement = document.createElement("p");
+  const textElement = document.createElement("p");
+  const dateElement = document.createElement("p");
+  const date = new Date().toLocaleTimeString();
+  const index = date.lastIndexOf(":");
 
-    if (position != 'middle') {
-        nameElement.innerText = message.substring(0, message.indexOf(':'));
-        nameElement.classList.add('sender-name');
-        messageElement.appendChild(nameElement);
-    }
+  if (position != "middle") {
+    nameElement.innerText = message.substring(0, message.indexOf(":"));
+    nameElement.classList.add("sender-name");
+    messageElement.appendChild(nameElement);
+  }
 
-    const messageStartIndex = (position != 'middle')? message.indexOf(':') + 2 : 0;
-    textElement.innerText = message.substring(messageStartIndex);
-    messageElement.appendChild(textElement);
+  const messageStartIndex = position != "middle" ? message.indexOf(":") + 2 : 0;
+  textElement.innerText = message.substring(messageStartIndex);
+  textElement.classList.add("message-text");
+  messageElement.appendChild(textElement);
 
-    if (position != 'middle') {
-        dateElement.innerText = date.substring(0, index) + date.substring(index + 3);
-        dateElement.classList.add('date');
-        messageElement.appendChild(dateElement);
-    }
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
-    messageContainer.appendChild(messageElement);
+  if (position != "middle") {
+    dateElement.innerText =
+      date.substring(0, index) + date.substring(index + 3);
+    dateElement.classList.add("date");
+    messageElement.appendChild(dateElement);
+  }
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.appendChild(messageElement);
 }
-
--- server/server.js
+++ server/server.js
@@ -1,27 +1,31 @@
 const port = 3000;
-const io = require('socket.io')(port);
+const io = require("socket.io")(port);
 
 const users = {};
 
-io.on('connection', socket => {
-    socket.on('new-user', name => {
-        if(name == null){
-            return;
-        }
-        console.log(name + " joined");
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    if (name == null) {
+      return;
+    }
+    console.log(name + " joined");
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
 
-    socket.on('send', message => {
-        const data = { name: users[socket.id], message: message };
-        socket.broadcast.emit('receive', data);
-    });
+  socket.on("send", (message) => {
+    const data = { name: users[socket.id], message: message };
+    socket.broadcast.emit("receive", data);
+  });
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('disconnected', users[socket.id]);
-        delete users[socket.id];
-    });
+  socket.on("disconnect", () => {
+    if (users[socket.id] == null) {
+      return;
+    }
+    console.log(users[socket.id] + " left");
+    socket.broadcast.emit("disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
 });
 
 console.log(`Listening at port ${port}`);
